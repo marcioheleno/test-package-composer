26210f8dbb485c1cb230d57402baaefe8e060a17
marcioheleno <marcio@marcioheleno.website>
2016-09-14 00:05:55 -0300

    commit inicial

---
+++ readme.md
@@ -0,0 +1,1 @@
+**Teste de package do composer**
---
+++ src/marcio/pack/HelloPackage.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: marcioheleno
+ * Date: 13/09/16
+ * Time: 23:36
+ */
+
+namespace marcio\pack;
+
+
+class HelloPackage
+{
+    public function getHello()
+    {
+        return "Hello Package!";
+    }
+}
